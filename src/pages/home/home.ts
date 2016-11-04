ffe144140479f913af793b12b43c8afbb2069a88
hicks011 <hicksbo@yahoo.com>
2016-11-04 13:34:28 -0400

    update2
    working
    json-server http://task.lafrime.net/artists/api.php/artists

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,64 +1,32 @@
 import { Component } from '@angular/core';
-//import { NavController, ModalController } from 'ionic-angular';
-import {NavController} from 'ionic-angular';
-import {AddPage} from '../add-page/add-page';
-//import { Weather } from '../../providers/weather';
+import { Http } from '@angular/http';
+import { NavController } from 'ionic-angular';
 import 'rxjs/add/operator/map';
-//import { Temperature } from '../../pipes/temperature';
-//import { Forecast } from '../forecast/forecast';
-import {http} from '@angular/core';
-
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html',
-//  providers: [Weather]
-
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-  //public weatherList = [];
-items
+  items
 
-  constructor(public navCtrl: NavController, private http: http){
-  this.http.get("http://localhost:3000/db").map(res => res.json()).subscribe(data => {
-    this.items = data;
-    console.log ("GOT RESULTS");
-  },
-  error => {
-    console.log("OOPS!");
-  });
-}
-
-    /*public modalCtrl: ModalController
-    public weather: Weather
-  ) { }
-
-  addWeather() {
-    let addWeatherModal = this.modalCtrl.create(AddPage);
-    addWeatherModal.onDidDismiss((data) => {
-
-      if (data) {
-        this.getWeather(data.city, data.country);
-      }
-    });
-    addWeatherModal.present(AddPage);
+  constructor(public navCtrl: NavController, private http: Http) {
+        //this.http.get("http://task.lafrime.net/artists/api.php/artists").map(res => res.json()).subscribe(data => {
+        //this.http.get("https://api.randomuser.me/?results=10").map(res => res.json()).subscribe(data => {
+        this.http.get("http://localhost:3000/db").map(res => res.json()).subscribe(data => {
+        //this.http.get("https://randomuser.me/api/?results").map(res => res.json()).subscribe(data => {
+        this.items = data;
+        //this.items.push(data);
+        console.log("GOT RESULTS");
+        },
+        error => {
+          console.log("OOPS!");
+        });
   }
 
-  getWeather(city: string, country: string) {
-    this.weather.city(city, country)
-      .map(data => data.json())
-      .subscribe(data => {
-        this.weatherList.push(data);
-      },
-      err => console.log(err),
-      () => console.log('Weather Here'))
+  itemClicked(event, item) {
+    console.log(item.title);
   }
 
-
-  viewForecast(cityWeather) {
-    this.navCtrl.push(Forecast, { cityWeather: cityWeather });
-  }
-}
-*/
 }
